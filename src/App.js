05b8df28044881390f5c15db6d5978153ca092fd
Chris Jang <chris3jang@gmail.com>
2019-01-16 22:38:48 -0500

    dynamic form

--- src/App.js
+++ src/App.js
@@ -3,19 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <form action="">
-        	<p>Band Name:</p>
-        	<input type="text" name="bandname" placeholder="name"></input>
-        	<p>Member:</p>
-        	<input type="text" name="bandname" placeholder="name"></input>
-        	<input type="text" name="bandname" placeholder="instrument"></input>
-        	<button>+</button>
-        </form>
-      </div>
-    );
+
+  	state = {count: 1};
+
+  	handleAddMember(e) {
+  		e.preventDefault();
+  		let oldCount = this.state.count;
+  		this.setState({count: oldCount + 1});
+  	}
+
+  	
+
+	render() {
+		let formFields = [];
+	  	for(let i = 0; i < this.state.count; i++) {
+	  		formFields.push(
+	  			<div>
+					<input type="text" name="memberbame" placeholder="name"></input>
+					<input type="text" name="instrument" placeholder="instrument"></input>
+					<input type="text" name="time" placeholder="timeframe"></input>
+				</div>
+	  		);
+	  	}
+
+	    return (
+	      <div className="App">
+	        <form action="">
+	        	<p>Band Name:</p>
+	        	<input type="text" name="bandname" placeholder="name"></input>
+	        	<p>Members:</p>
+	        	<button onClick={this.handleAddInstrument.bind(this)}>+</button>
+	        	<button onClick={this.handleAddTimeframe.bind(this)}>+</button>
+	        	{formFields}
+	        	<button style={{display: 'block', margin: 'auto', marginTop: 10}} onClick={this.handleAddMember.bind(this)}>Add Member</button>
+	        </form>
+	      </div>
+	    );
   }
 }
 
