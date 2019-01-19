45ac80b030e4b5eb67c45742b07e89b6718a0012
Chris Jang <chris3jang@gmail.com>
2019-01-18 20:38:08 -0500

    add instr/timefrm

--- src/App.js
+++ src/App.js
@@ -4,38 +4,107 @@ import './App.css';
 
 class App extends Component {
 
-  	state = {count: 1};
+	numberMemberLine(num) {
+		return (
+			<div id={num}>
+        {[
+          [
+				  <input type="text" name="memberbame" placeholder="name"></input>
+          ], 
+          [
+				  <div style={{display: 'inline-block', width: 20}}></div>
+          ],
+          [
+          <div style={{display: 'inline-block'}}>
+				  <button onClick={this.handleAddInstrument.bind(this)}>+</button>
+				  <input key={num} type="text" name="instrument" placeholder="instrument"></input>
+          </div>
+          ],
+          [
+				  <div style={{display: 'inline-block', width: 20}}></div>
+          ],
+          [
+          <div style={{display: 'inline-block'}}>
+				  <button onClick={this.handleAddTimeframe.bind(this)}>+</button>
+				  <input type="text" name="time" placeholder="timeframe"></input>
+          </div>
+          ]
+        ]}
+			</div>
+		)
+	}
+
+  	state = {
+  		forms: [
+  			[this.numberMemberLine(0)]
+  		] 
+  	};
 
   	handleAddMember(e) {
   		e.preventDefault();
-  		let oldCount = this.state.count;
-  		this.setState({count: oldCount + 1});
+  		this.setState(prevState => ({
+  			forms: [...prevState.forms, 
+  				[this.numberMemberLine(prevState.forms.length)]
+  			]}));
+  	}
+
+  	handleAddInstrument(e) {
+  		e.preventDefault();
+  		const index = e.currentTarget.parentElement.id;
+
+  		this.setState(prevState => {
+  			const forms = prevState.forms.map((item, i) => {
+  				if(i == index) {
+            console.log("item", item)
+  					return item.concat(
+              <div style={{display: 'block'}}>
+  						  <input type="text" name="instrument" placeholder="instrument" style={{}}></input>
+              </div>
+  					);
+  				}
+  				else return item;
+  			})
+  			return {
+  				forms: forms
+  			}
+  		});
+  	}
+
+  	handleAddTimeframe(e) {
+  		e.preventDefault();
+      const index = e.currentTarget.parentElement.id;
+
+      this.setState(prevState => {
+        const forms = prevState.forms.map((item, i) => {
+          if(i == index) {
+            console.log("item", item)
+            return item.concat(
+              <div style={{display: 'block'}}>
+                <input type="text" name="time" placeholder="timeframe" style={{}}></input>
+              </div>
+            );
+          }
+          else return item;
+        })
+        return {
+          forms: forms
+        }
+      });
   	}
 
   	
 
 	render() {
-		let formFields = [];
-	  	for(let i = 0; i < this.state.count; i++) {
-	  		formFields.push(
-	  			<div>
-					<input type="text" name="memberbame" placeholder="name"></input>
-					<input type="text" name="instrument" placeholder="instrument"></input>
-					<input type="text" name="time" placeholder="timeframe"></input>
-				</div>
-	  		);
-	  	}
-
 	    return (
 	      <div className="App">
 	        <form action="">
 	        	<p>Band Name:</p>
 	        	<input type="text" name="bandname" placeholder="name"></input>
-	        	<p>Members:</p>
-	        	<button onClick={this.handleAddInstrument.bind(this)}>+</button>
-	        	<button onClick={this.handleAddTimeframe.bind(this)}>+</button>
-	        	{formFields}
-	        	<button style={{display: 'block', margin: 'auto', marginTop: 10}} onClick={this.handleAddMember.bind(this)}>Add Member</button>
+            <div>
+              <button style={{display: 'inline-block', margin: 'auto', marginTop: 10}} onClick={this.handleAddMember.bind(this)}>+</button>
+	        	  <p style={{display: 'inline-block'}}>Members:</p>
+            </div>
+	        	{this.state.forms}
 	        </form>
 	      </div>
 	    );
